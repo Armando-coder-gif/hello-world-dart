4294c994ea49d0e98f6e76adef488bfe52fd1237
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2024-03-21 21:14:23 -0600

    removing debug tag

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -6,10 +6,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Center(child: Text("Hola mundo")),
+      debugShowCheckedModeBanner: false, // quita la etiqueta de debug en la pantalla
+      home: Scaffold(
+        body:Center(child: Text("Hola mundo")),
+      ),
 
     );
   }
