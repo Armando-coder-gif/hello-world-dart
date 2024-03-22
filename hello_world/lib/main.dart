abcbbb4135cdb463dacc2376dc9e8061fe2940a6
Armando Gonzalez <armando.gonzalez@correo.unimet.edu.ve>
2024-03-21 23:00:25 -0600

    appBar Acciones

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/presentation/screens/counter/counter_screen.dart';
+import 'package:hello_world/presentation/screens/counter/counter_functions_screen.dart';
+// import 'package:hello_world/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   // widget inicial
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true, // se pueden editar los widgets como aparecen en material3
         colorSchemeSeed: Colors.blue, // crea una paleta de colores
       ),
-      home: const CounterScreen(),
+      home: const CounterFunctionsScreen(),
 
     );
   }
